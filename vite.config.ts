ca6b7f75984e1fc14fa4183d7e3b294db4005eea
choonewza-pro <choonewza@gmail.com>
2025-11-14 14:27:44 +0700

    feat: add shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,5 +11,10 @@ export default defineConfig(({ mode }) => {
   return {
     base,
     plugins: [tailwindcss(), react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   };
 });
